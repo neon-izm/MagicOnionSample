85b5c37326d73af3a640d0a97c60e5d05dbc3d02
neon-izm <jj.y0sh1da@gmail.com>
2025-03-08 20:20:10 +0900

    test for cli

---
+++ src/MyApp.CliClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using MagicOnion.Client;
+using MyApp.Shared;
+
+// Connect to the server using gRPC channel.
+var channel = GrpcChannel.ForAddress("http://127.0.0.1:5018");
+
+// Create a proxy to call the server transparently.
+var client = MagicOnionClient.Create<IMyFirstService>(channel);
+
+// Call the server-side method using the proxy.
+var result = await client.SumAsync(123, 456);
+Console.WriteLine($"Result: {result}");
